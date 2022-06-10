715a50a1fffc2c1a677c4f7f1bebcab2deb3efcd
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2022-06-10 18:48:29 +0530

    Added in the table.

--- src/components/MyRewards.js
+++ src/components/MyRewards.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 import { useMoralis } from 'react-moralis';
 
-import CustomTable from './CustomTable'
+import CustomTable from './CustomTable';
+import BlueButton from './BlueButton';
 
 import rewardPoolContractAbi from "../assets/blockchain/reward_pool_abi.json";
 import erc20Abi from "../assets/blockchain/erc20_abi.json";
@@ -10,7 +11,8 @@ import { ethers } from 'ethers';
 
 const HEADERS = [
   'Name',
-  'Min Balance Required',
+  // 'Min Balance Required',
+  'Current Status',
   'Total Rewarded',
   'Claimable',
   'Next Claim',
@@ -19,51 +21,97 @@ const HEADERS = [
 
 const MyRewards = () => {
   const [isLoading, setIsLoading] = React.useState(true);
-  const { isAuthenticated, isAuthenticating, user, account, Moralis} = useMoralis();
+  const [tablerows, setTablerows] = React.useState([]);
+  const { isAuthenticated, isAuthenticating, user, account, Moralis } = useMoralis();
   const t = useMoralis();
+
+  let rows = [];
 
   // console.log("Signer", await signer.getAddress())
 
   React.useEffect(() => {
-    async function fetchData(){
-      
-    // console.log("is Authenticating?", isAuthenticating);
-    // console.log("is Authenticated?", isAuthenticated);
+    async function fetchData() {
 
-    // console.log("account?", account);
-    if(isAuthenticated) {
-      console.log("is loading?", isLoading)
-      const provider = await Moralis.enableWeb3();
-      const signer = provider.getSigner(account);
-  
-      console.log("Signer", await signer.getAddress())   
-      console.log("Totality", t)
-      const rewardPoolContract = new ethers.Contract(
-        "0x0F7eB0cE0803Ac8aA1799777797B3db90ecACcAF",
-        rewardPoolContractAbi,
-        signer
-      );
-      let promiseArr = [];
-      console.log("Starting promise");
+      // console.log("is Authenticating?", isAuthenticating);
+      // console.log("is Authenticated?", isAuthenticated);
 
-      for(let i = 0 ; i< 10 ; i++ ) {
-        // promiseArr.push( async () => {
-          console.log("Pushing promise",i);
-          const poolAssetAddress = await rewardPoolContract.rewardAssetAt(i);
-          console.log("Reward Pool Asset", poolAssetAddress, i)
-          const erc20Contract = new ethers.Contract(
-            poolAssetAddress,
-            erc20Abi,
-            provider
-          )
-          console.log("Reward Pool Asset name", await  erc20Contract.name(), i);
-        // })
+      // console.log("account?", account);
+      if (isAuthenticated) {
+        // console.log("is loading?", isLoading)
+        const provider = await Moralis.enableWeb3();
+        const signer = provider.getSigner(account);
+
+        // console.log("Signer", await signer.getAddress())
+        // console.log("Totality", t)
+        const rewardPoolContract = new ethers.Contract(
+          "0x0F7eB0cE0803Ac8aA1799777797B3db90ecACcAF",
+          rewardPoolContractAbi,
+          signer
+        );
+
+        let promiseArr = [], results;
+
+        for (let i = 0; i < 10; i++) {
+          promiseArr.push(rewardPoolContract.rewardAssetAt(i))
+        }
+        const tokenAddresses = await Promise.all(promiseArr);
+
+        promiseArr = [];
+        for (let i = 0; i < 10; i++) {
+          if(tokenAddresses[i] !== "0x0000000000000000000000000000000000000000"){
+            const erc20Contract = new ethers.Contract(
+              tokenAddresses[i],
+              erc20Abi,
+              signer
+            );
+            promiseArr.push(erc20Contract.name())
+          }
+        }
+        const tokenNames = await Promise.all(promiseArr);
+
+        const totalTokens = tokenNames.length;
+
+        promiseArr = [];
+
+        for (let i = 0; i < totalTokens; i++) {
+          promiseArr.push(rewardPoolContract.rewardInfo(tokenAddresses[i]))
+        }
+
+        const currentStatus = (await Promise.all(promiseArr)).map(({minimumTokenBalanceForRewards}) => (ethers.utils.formatEther(minimumTokenBalanceForRewards.toString())));
+
+        promiseArr = [];
+
+        for (let i = 0; i < totalTokens; i++) {
+          promiseArr.push(rewardPoolContract.withdrawnRewardOf(tokenAddresses[i], account))
+        }
+
+        const totalRewarded = (await Promise.all(promiseArr)).map(each => ethers.utils.formatEther(each.toString()));
+
+        promiseArr = [];
+
+        for (let i = 0; i < totalTokens; i++) {
+          promiseArr.push(rewardPoolContract.withdrawableRewardOf(tokenAddresses[i], account))
+        }
+
+        const claimable = (await Promise.all(promiseArr)).map(each => ethers.utils.formatEther(each.toString()));
+
+        promiseArr = [];
+
+        for (let i = 0; i < totalTokens; i++) {
+          promiseArr.push(rewardPoolContract.getAccountRewardsInfo(tokenAddresses[i], account))
+        }
+
+        const nextClaim = (await Promise.all(promiseArr)).map(({nextClaimTime}) => nextClaimTime.toString());
+
+        
+        for(let i = 0 ; i < totalTokens ; i++){
+          rows.push([tokenNames[i], currentStatus[i], totalRewarded[i], claimable[i], nextClaim[i], <BlueButton onClick={() => rewardPoolContract.singleRewardClaimByUser(tokenAddresses[i])}>Claim</BlueButton>])
+        }
+
+        setTablerows(rows)
+        setIsLoading(prev => false)
+
       }
-      // Promise.allSettled(promiseArr).then( (results) => 
-      // {setIsLoading(prev => false); console.log("All settled")});
-      setIsLoading(prev => false)
-
-    }
 
 
 
@@ -74,7 +122,7 @@ const MyRewards = () => {
   }, [isAuthenticated])
 
   return (
-    <CustomTable headers={HEADERS} isLoading={isLoading}/>
+    <CustomTable headers={HEADERS} isLoading={isLoading} tablerows={tablerows}/>
   )
 }
 
