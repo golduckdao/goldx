4971f391c9affe04b29be0cd4126297c7a6b8063
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2022-08-26 22:07:47 +0530

    Added cosmetic changes.

--- src/components/ActionCard.js
+++ src/components/ActionCard.js
@@ -5,6 +5,9 @@ import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
 
 export default function ActionCard({children, img, alt, noImg, link}) {
+  const handleClick = (link) => {
+    if(link) window.open(link, "_blank")
+  }
   return (
     <Card sx={{
       maxWidth: 240,
@@ -13,17 +16,21 @@ export default function ActionCard({children, img, alt, noImg, link}) {
       background: '#1A2662',
       boxShadow: '5px 0px 8px rgba(78, 0, 118, 0.8), -5px 0px 8px rgba(0, 68, 101, 0.8)'
     }}>
-      <CardActionArea sx={{ height: '100%'}}
-        href={link}
+      <CardActionArea sx={{ height: '100%', pt: 5}}
+        component="button"
+        href={() => handleClick(link)}
         rel="no-referrer"
         target="_blank"
       >
         { !noImg &&
         <CardMedia
           component="img"
-          height="140"
           image={img}
           alt={alt}
+          height="60"
+          sx={{
+            objectFit: 'contain'
+          }}
         />
         }
         <CardContent>
